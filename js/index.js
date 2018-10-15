bb24ff4880678d578971abae2da127128dc2b2e0
Stijn van Schooten <svsoke@hotmail.com>
2018-10-15 18:40:54 +0200

    foundation

---
+++ js/index.js
@@ -0,0 +1,57 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBa78Det6d2GObTrsBU1roZ0P3P0SKHONQ",
+    authDomain: "svanschooten-c01f7.firebaseapp.com",
+    databaseURL: "https://svanschooten-c01f7.firebaseio.com",
+    projectId: "svanschooten-c01f7",
+    storageBucket: "svanschooten-c01f7.appspot.com",
+    messagingSenderId: "211466426536"
+};
+
+const config = {
+    scrollInterrupt: 300,
+    bannerFade: 5 * 1000,
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+firestore.settings({
+    timestampsInSnapshots: true
+});
+
+let posts = [];
+let projects = [];
+let statuses = [];
+let subjects = [];
+let types = [];
+Vue.use(VueMarkdown);
+const app = new Vue({
+    el: '#app',
+    data: {
+        posts: posts,
+        projects: projects,
+        statuses: statuses,
+        subjects: subjects,
+        types: types
+    }
+});
+
+async function finishSetup() {
+    console.log("finishing setup");
+}
+
+(async () => {
+    await setupNavigation();
+    await setupBanner();
+    await getFirestoreData(firestore, "statuses", statuses);
+    await getFirestoreData(firestore, "subjects", subjects);
+    await getFirestoreData(firestore, "types", types);
+    await getFirestoreData(firestore, "projects", projects, (project) => {
+        project.content = atob(project.content);
+        return project;
+    });
+    await getFirestoreData(firestore, "blog", posts, (post) => {
+        post.content = atob(post.content);
+        return post;
+    });
+    await finishSetup();
+})();
---
+++ js/util.js
@@ -0,0 +1,65 @@
+let pages = [];
+let pageNavigationElements = [];
+const titles = ["Home", "Projects", "Blog", "Contact"];
+const banners = ["banner-1.jpg", "banner-2.jpg", "banner-3.jpg", "banner-4.jpg", "banner-5.jpg", "banner-6.jpg", "banner-7.jpg"];
+let scrolling = false;
+
+function scrollTo(index) {
+    if (scrolling) return;
+    scrolling = true;
+    setTimeout(() => {
+        scrolling = false;
+    }, config.scrollInterrupt);
+    document.title = `${titles[index]} | Stijn van Schooten | svanschooten.nl`;
+    pages[index].scrollIntoView({
+        behavior: "smooth"
+    });
+    for (let pageNavigationElement of pageNavigationElements) {
+        pageNavigationElement.classList.remove("active");
+    }
+    pageNavigationElements[index].classList.add("active");
+}
+
+async function setupNavigation() {
+    pages = document.getElementsByClassName("page");
+    pageNavigationElements = document.getElementsByClassName("page-navigation-element");
+
+    for (let i = 0; i < pages.length; i++) {
+        let page = pages[i];
+        let pageNavigationElement = pageNavigationElements[i];
+        page.addEventListener("wheel", (event) => {
+            if (event.deltaY > 0 && i < (pages.length - 1)) {
+                scrollTo(i + 1);
+            } else if (event.deltaY < 0 && i > 0) {
+                scrollTo(i - 1);
+            }
+        });
+        pageNavigationElement.addEventListener("click", () => {
+            scrollTo(i);
+        });
+    }
+
+    document.getElementById("container").style.width = `${pages.length * 100}%`;
+}
+
+async function setupBanner() {
+    const banner = banners[getRandom(banners.length)];
+    document.getElementById("header").style.backgroundImage = `url('images/banners/${banner}')`;
+}
+
+function getRandom(length) {
+    return Math.floor(Math.random() * length);
+}
+
+async function getFirestoreData(fs, collection, array, transform) {
+    fs.collection(collection).get().then((querySnapshot) => {
+        querySnapshot.forEach((snapshotData) => {
+            let item = snapshotData.data();
+            item.id = snapshotData.id;
+            if (transform) {
+                item = transform(item);
+            }
+            array.push(item);
+        });
+    });
+}
